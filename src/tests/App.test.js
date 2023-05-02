bc429eaa8f6933177df9bd3bbcc577942525d5fc
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-05-02 17:08:12 -0300

    Testando os links e seus textos na rota app.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,10 +5,24 @@ import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
 describe('Testando as rotas do app.', () => {
-  it('Testa se a aplicação é renderizada inicialmente na rota correta.', () => {
-    const { history } = renderWithRouter(<App />);
-    const { location: { pathname } } = history;
-    expect(pathname).toEqual('/');
-  });
+  // it('Testa se a aplicação é renderizada inicialmente na rota correta.', () => {
+  //   const { history } = renderWithRouter(<App />);
+  //   const { location: { pathname } } = history;
+  //   expect(pathname).toEqual('/');
+  // });
 
+  // it('Testa se a aplicação contém quatro links para navegação quando esta na rota home.', () => {
+  //   renderWithRouter(<App />);
+  //   const linksApp = screen.getAllByRole('link');
+  //   expect(linksApp.length).toBe(4);
+  // });
+
+  it('Testa se os links para a rotas comtém os textos corretos.', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getAllByRole('link');
+    expect(linkHome[0].textContent).toBe('Home');
+    expect(linkHome[1].textContent).toBe('About');
+    expect(linkHome[2].textContent).toBe('Favorite Pokémon');
+    expect(linkHome[3].textContent).toBe('More details');
+  });
 });
