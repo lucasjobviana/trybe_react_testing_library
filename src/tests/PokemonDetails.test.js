fdba9b0b65bead5c317ce1dfc3cf6eb5c0d33ffc
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-05-05 14:45:21 -0300

    Finalizando ultimo requisito.

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { screen, act } from '@testing-library/react';
 
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 import pokemonList from '../data';
 
 describe('Testando os elementos da página "PokemonDetails.js', () => {
-  it('d', () => {
-    // console.log(pokemonList[0]);
+  it('Fazendo testes', () => {
     const { history } = renderWithRouter(<App />);
-    // console.log(pokemonList[0]);
     console.log(pokemonList[0]);
     const { name, id, summary, foundAt } = pokemonList[0];
     act(() => { history.push(`/pokemon/${id}`); });
@@ -18,8 +17,17 @@ describe('Testando os elementos da página "PokemonDetails.js', () => {
     screen.getByRole('heading', { level: 2, name: 'Summary' });
     screen.getByText(summary);
     screen.getByRole('heading', { level: 2, name: `Game Locations of ${name}` });
-    foundAt.forEach(({ location }) => {
+    foundAt.forEach(({ location, map }, index) => {
       screen.getByText(location);
+      const imgLocations = screen.getAllByAltText(`${name} location`);
+      expect(imgLocations[index].src).toEqual(map);
     });
+    screen.getByText('Pokémon favoritado?');
+    const check = screen.queryByRole('checkbox');
+    expect(check.checked).toEqual(false);
+    userEvent.click(check);
+    expect(check.checked).toEqual(true);
+    userEvent.click(check);
+    expect(check.checked).toEqual(false);
   });
 });
